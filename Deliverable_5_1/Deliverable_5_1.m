7aa7a88e06b4da890e0f65354b6e99b0963adefa
Francesco Maglie <74652100+fra-mgl@users.noreply.github.com>
2023-12-17 15:15:14 +0100

    deliv 5.1 ok

--- Deliverable_5_1/Deliverable_5_1.m
+++ Deliverable_5_1/Deliverable_5_1.m
@@ -1,7 +1,45 @@
-addpath(fullfile('..', 'src'));
+addpath(fullfile('.', 'src'));
+addpath(fullfile('.', 'Deliverable_5_1/'));
 
-%close all
-%clear all
-%clc
+close all
+clear all
+clc
 
-%% TODO: This file should produce all the plots for the deliverable
+% %% TODO: This file should produce all the plots for the deliverable
+
+Ts = 1/20; % Sample time
+Tf = 8; % simulation end time
+
+rocket = Rocket(Ts);
+[xs, us] = rocket.trim();
+sys = rocket.linearize(xs, us);
+[sys_x, sys_y, sys_z, sys_roll] = rocket.decompose(sys, xs, us);
+H = 7; % Horizon length in seconds
+
+
+%%
+clc;
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+
+
+% Merge four sub−system controllers into one full−system controller
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
+
+% simulate with disturbance
+x0 = [zeros(1, 9), 1 0 3]';
+ref = [1.2, 0, 3, 0]';
+
+rocket.mass = 2.13;
+
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
+% Visualize
+rocket.anim_rate = 1; % Increase this to make the animation faster
+ph = rocket.plotvis(T, X, U, Ref);
+ph.fig.Name = 'Merged lin. MPC in nonlinear simulation'; % Set a figure title
+
+
+% [T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+% Z_hat(end,:)
