86738d24db5bfc49eaf53a67a78ab627148926bc
Francesco Maglie <74652100+fra-mgl@users.noreply.github.com>
2023-12-21 16:16:23 +0100

    fix constraint on z controller in dev 5.2

--- Deliverable_5_2/MpcControl_z.m
+++ Deliverable_5_2/MpcControl_z.m
@@ -57,7 +57,8 @@ classdef MpcControl_z < MpcControlBase
             % input constraints
             G = [1 -1]';
             % g = [80 -50]';
-            g = [23.3333 36.6667]';
+            % g = [23.3333 36.6667]';
+            g = [23.3333 21.167]';
 
             % Q = diag([1000 10000]);
             % R = 0.1;
@@ -122,7 +123,8 @@ classdef MpcControl_z < MpcControlBase
             % input constraints
             G = [1 -1]';
             % g = [80 -50]';
-            g = [23.3333 36.6667]';
+            % g = [23.3333 36.6667]';
+            g = [23.3333 21.167]';
 
             % compute steady-state considering disturbance
             Bd = mpc.B;
