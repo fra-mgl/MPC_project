039fa02c52eee5e81615bc8db30d21a0afed07f1
Chiara Evangelisti <s282113@studenti.polito.it>
2023-12-30 23:08:42 +0100

    Tryied to add plots from 4.1, NOT SUCCESSFULL

--- Deliverable_5_1/Deliverable_5_1.m
+++ Deliverable_5_1/Deliverable_5_1.m
@@ -7,7 +7,7 @@ clc
 % %% TODO: This file should produce all the plots for the deliverable
 
 Ts = 1/20; % Sample time
-Tf = 20; % simulation end time
+Tf = 8; % simulation end time
 
 rocket = Rocket(Ts);
 [xs, us] = rocket.trim();
@@ -17,7 +17,15 @@ H = 7; % Horizon length in seconds
 
 
 %%
-clc;
+
+% simulate with disturbance
+x0 = [zeros(1, 9), 1 0 3]';
+ref = [1.2, 0, 3, 0]';
+
+rocket.mass = 2.13;
+
+%SIMULATE WITH CONTROLLER FROM DELIVERABLE 4
+addpath(fullfile('..', 'Deliverable_4_1'));
 mpc_x = MpcControl_x(sys_x, Ts, H);
 mpc_y = MpcControl_y(sys_y, Ts, H);
 mpc_z = MpcControl_z(sys_z, Ts, H);
@@ -27,21 +35,30 @@ mpc_roll = MpcControl_roll(sys_roll, Ts, H);
 % Merge four sub−system controllers into one full−system controller
 mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
 
-% simulate with disturbance
-x0 = [zeros(1, 9), 1 0 3]';
-ref = [1.2, 0, 3, 0]';
+[T4, X4, U4, Ref, Z_hat4] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
 
-rocket.mass = 2.13;
+% Visualize
+rocket.anim_rate = 1; % Increase this to make the animation faster
+ph4 = rocket.plotvis(T4, X4, U4, Ref);
+ph4.fig.Name = 'Merged lin. MPC  disturbance without estimator'; % Set a figure title
 
-%[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
+rmpath(fullfile('..', 'Deliverable_4_1'));
+
+
+%SIMULATE WITH UPDATED FROM DELIVERABLE 4
+mpc_x = MpcControl_x(sys_x, Ts, H);
+mpc_y = MpcControl_y(sys_y, Ts, H);
+mpc_z = MpcControl_z(sys_z, Ts, H);
+mpc_roll = MpcControl_roll(sys_roll, Ts, H);
+
+% Merge four sub−system controllers into one full−system controller
+mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
 
 [T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
 
 % Visualize
 rocket.anim_rate = 1; % Increase this to make the animation faster
 ph = rocket.plotvis(T, X, U, Ref);
-ph.fig.Name = 'Merged lin. MPC in nonlinear simulation'; % Set a figure title
+ph.fig.Name = 'Merged lin. MPC ioffset free tracking'; % Set a figure title
 
-
-% [T, X, U, Ref, Z_hat] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
-% Z_hat(end,:)
+%[T, X, U, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
