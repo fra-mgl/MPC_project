0d51b0224f22b3bc41ebda4620b498e3e5d6511a
Francesco Maglie <74652100+fra-mgl@users.noreply.github.com>
2023-12-31 08:27:50 +0100

    fix path for Dev4.1 in Dev5.1

--- Deliverable_5_1/Deliverable_5_1.m
+++ Deliverable_5_1/Deliverable_5_1.m
@@ -25,27 +25,32 @@ ref = [1.2, 0, 3, 0]';
 rocket.mass = 2.13;
 
 %SIMULATE WITH CONTROLLER FROM DELIVERABLE 4
-addpath(fullfile('..', 'Deliverable_4_1'));
+cd(fullfile('..', 'Deliverable_4_1'));
+% change current directory to access controllers in Deliverable 4.1
+disp("Simulating using controllers from Deliverable 4.1");
 mpc_x = MpcControl_x(sys_x, Ts, H);
 mpc_y = MpcControl_y(sys_y, Ts, H);
 mpc_z = MpcControl_z(sys_z, Ts, H);
 mpc_roll = MpcControl_roll(sys_roll, Ts, H);
-
+cd(fullfile('..', 'Deliverable_5_1'));
+% restore current directory
 
 % Merge four sub−system controllers into one full−system controller
 mpc = rocket.merge_lin_controllers(xs, us, mpc_x, mpc_y, mpc_z, mpc_roll);
 
-[T4, X4, U4, Ref, Z_hat4] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+% [T4, X4, U4, Ref, Z_hat4] = rocket.simulate_est_z(x0, Tf, @mpc.get_u, ref, mpc_z, sys_z);
+[T4, X4, U4, Ref] = rocket.simulate(x0, Tf, @mpc.get_u, ref);
 
 % Visualize
 rocket.anim_rate = 1; % Increase this to make the animation faster
 ph4 = rocket.plotvis(T4, X4, U4, Ref);
 ph4.fig.Name = 'Merged lin. MPC  disturbance without estimator'; % Set a figure title
 
-rmpath(fullfile('..', 'Deliverable_4_1'));
+% rmpath(fullfile('..', 'Deliverable_4_1'));
 
 
 %SIMULATE WITH UPDATED FROM DELIVERABLE 4
+disp("Simulating using controllers from Deliverable 5.1");
 mpc_x = MpcControl_x(sys_x, Ts, H);
 mpc_y = MpcControl_y(sys_y, Ts, H);
 mpc_z = MpcControl_z(sys_z, Ts, H);
