60a0480b7f73b24b7933a5e5a66543173de5f5c8
Francesco Maglie <74652100+fra-mgl@users.noreply.github.com>
2024-01-11 11:36:37 +0100

    add test 4 in Deliverable 6.2

--- Deliverable_6_2/Deliverable_6_2.m
+++ Deliverable_6_2/Deliverable_6_2.m
@@ -63,3 +63,22 @@ rocket.anim_rate = 1;
 ph = rocket.plotvis(T, X , U , Ref);
 ph.fig.Name = 'Partially delay-compensating controller, expected delay=0.025s, actual delay = 0.075s, horizon H=2.5s'; % Set a figure title
 
+%% TEST 4
+Tf = 5; % simulation time
+rocket = Rocket(Ts);
+H = 2.5; % Horizon length in seconds
+rocket.mass = 1.75;
+
+expected_delay = 1; % number of steps
+rocket.delay = 4; % 0 if not specified, ACTUAL DELAY
+
+nmpc = NmpcControl(rocket, H, expected_delay);
+x0 = zeros(12, 1);
+ref = [0.5, 0, 1, deg2rad(65)]';
+
+% SIMULATION
+rocket.anim_rate = 1;
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+ph = rocket.plotvis(T, X , U , Ref);
+ph.fig.Name = 'Partially delay-compensating controller, expected delay=0.025s, actual delay = 0.1s, horizon H=2.5s'; % Set a figure title
+
