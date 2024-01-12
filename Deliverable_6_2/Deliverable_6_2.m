113920b73ce378c702f97023cc30484d2367ec22
Chiara Evangelisti <s282113@studenti.polito.it>
2024-01-12 09:47:03 +0100

    6.2 finished

--- Deliverable_6_2/Deliverable_6_2.m
+++ Deliverable_6_2/Deliverable_6_2.m
@@ -7,9 +7,9 @@ clc
 Ts = 1/40; % Higher sampling rate for this part!
 
 %% TEST 1
-Tf = 7; % simulation time
+Tf = 8; % simulation time
 rocket = Rocket(Ts);
-H = 1.5; % Horizon length in seconds
+H = 1; % Horizon length in seconds
 rocket.mass = 1.75;
 
 expected_delay = 0; % number of steps
@@ -28,11 +28,11 @@ ph.fig.Name = 'Non-compensated controller, delay=0.05s, horizon H=1.5s'; % Set a
 %% TEST 2
 Tf = 5; % simulation time
 rocket = Rocket(Ts);
-H = 1.5; % Horizon length in seconds
+H = 1; % Horizon length in seconds
 rocket.mass = 1.75;
 
 expected_delay = 0; % number of steps
-rocket.delay = 4; % 0 if not specified, ACTUAL DELAY
+rocket.delay = 3; % 0 if not specified, ACTUAL DELAY
 
 nmpc = NmpcControl(rocket, H, expected_delay);
 x0 = zeros(12, 1);
@@ -42,12 +42,12 @@ ref = [0.5, 0, 1, deg2rad(65)]';
 rocket.anim_rate = 1;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 ph = rocket.plotvis(T, X , U , Ref);
-ph.fig.Name = 'Non compensating-compensating controller, delay=0.1s, horizon H=1s'; % Set a figure title
+ph.fig.Name = 'Non compensating-compensating controller, delay=0.75s, horizon H=1s'; % Set a figure title
 
 %% TEST 3
 Tf = 7; % simulation time
 rocket = Rocket(Ts);
-H = 1.5; % Horizon length in seconds
+H = 1; % Horizon length in seconds
 rocket.mass = 1.75;
 
 expected_delay = 5; % number of steps
@@ -61,17 +61,17 @@ ref = [0.5, 0, 1, deg2rad(65)]';
 rocket.anim_rate = 1;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 ph = rocket.plotvis(T, X , U , Ref);
-ph.fig.Name = 'Fully delay-compensating controller, delay=0.225s, horizon H=1s'; % Set a figure title
+ph.fig.Name = 'Fully delay-compensating controller, delay=0.2s, horizon H=1s'; % Set a figure title
 
 
 %% TEST 4
 Tf = 5; % simulation time
 rocket = Rocket(Ts);
-H = 1.5; % Horizon length in seconds
+H = 1; % Horizon length in seconds
 rocket.mass = 1.75;
 
 expected_delay = 1; % number of steps
-rocket.delay = 2; % 0 if not specified, ACTUAL DELAY
+rocket.delay =3; % 0 if not specified, ACTUAL DELAY
 
 nmpc = NmpcControl(rocket, H, expected_delay);
 x0 = zeros(12, 1);
@@ -81,5 +81,5 @@ ref = [0.5, 0, 1, deg2rad(65)]';
 rocket.anim_rate = 1;
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 ph = rocket.plotvis(T, X , U , Ref);
-ph.fig.Name = 'Partially delay-compensating controller, expected delay=0.025s, actual delay = 0.1s, horizon H=2.5s'; % Set a figure title
+ph.fig.Name = 'Partially delay-compensating controller, expected delay=0.025s, actual delay = 0.075s, horizon H=1s'; % Set a figure title
 
